befb9a6dc7d5ec5ce17706860c81656c8a98ee7f
Kesia23 <kesiasilvanunes@hotmail.com>
2020-09-17 16:59:33 -0400

    Project created

---
+++ backend/src/main/java/com/kesia/pesquisads/PesquisadsApplication.java
@@ -0,0 +1,13 @@
+package com.kesia.pesquisads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisadsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kesia/pesquisads/PesquisadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesia.pesquisads;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
